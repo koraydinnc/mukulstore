918a18e00041503180113a5db8e125055f4fdead
koraydinnc <koraydincc@icloud.com>
2025-01-15 23:15:21 +0300

    Fix

--- app/admin/layout.jsx
+++ app/admin/layout.jsx
@@ -1,10 +1,26 @@
+import { Poppins } from "next/font/google";
+import { Providers } from "./Providers";
+import { Toaster } from "@/components/ui/toaster";
+import './global.css';
+
+const poppins = Poppins({ 
+  weight: ['400', '500', '600', '700'],
+  subsets: ['latin'],
+  variable: '--font-poppins',
+});
 
 
 
 export default function AdminLayout({ children }) {
   return (
-    <div className="bg-gray-100 min-h-screen">
-      {children}
-    </div>
+<html lang="tr">
+     
+     <body className={poppins.className}>
+       <Providers>
+         {children}
+       </Providers>
+       <Toaster />
+     </body>
+   </html>
   );
 }
